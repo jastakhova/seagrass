9e7bdec539d3a3370ee740fd925bddadc2eeb335
Julia Astakhova <jastakhova@griddynamics.com>
2014-08-04 12:16:40 -0700

    navigation for metrics

--- www/js/app.js
+++ www/js/app.js
@@ -1,6 +1,18 @@
 (function() {
     var seagrass = angular.module("seagrass", ['onsen']);
 
+    seagrass.controller("MetricsController", [function () {
+        this.activeTab = 1;
+
+        this.chooseTab = function(tab) {
+          this.activeTab = tab;
+        };
+
+        this.isSelected = function(tab) {
+            return this.activeTab === tab;
+        };
+    }]);
+
     seagrass.controller("MembersController", ['$http', '$log', function ($http, $log) {
         var home = this;
 
